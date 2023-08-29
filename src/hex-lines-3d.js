635e7f9a70f0eb551fb8ae2440c347a61a0a64ad
Alan Cutter <randfur@gmail.com>
2023-08-30 07:58:08 +1000

    move

--- src/hex-lines-3d.js
+++ src/hex-lines-3d.js
@@ -1,34 +1,7 @@
-import {kLittleEndian, rgbaToUint32Flat, logIf} from './utils.js';
+import {kLittleEndian, rgbaToUint32, logIf} from './utils.js';
 import {buildVertexShader, kFragmentShader} from './shaders.js';
 
 export const kBytesPerHexPoint3d = 5 * 4;
-
-export function setHexPoint3dFlat(dataView, i, x, y, z, size, r, g, b, a) {
-  dataView.setFloat32(i * kBytesPerHexPoint3d + 0, x, kLittleEndian);
-  dataView.setFloat32(i * kBytesPerHexPoint3d + 4, y, kLittleEndian);
-  dataView.setFloat32(i * kBytesPerHexPoint3d + 8, z, kLittleEndian);
-  dataView.setFloat32(i * kBytesPerHexPoint3d + 12, size, kLittleEndian);
-  dataView.setUint32(i * kBytesPerHexPoint3d + 16, rgbaToUint32Flat(r, g, b, a), kLittleEndian);
-}
-
-export function clearHexPoint3d(dataView, i) {
-  dataView.setFloat32(i * kBytesPerHexPoint3d + 12, 0, kLittleEndian);
-}
-
-export function setHexPoint3d(dataView, i, hexPoint3d) {
-  if (hexPoint3d === null) {
-    clearHexPoint3d(dataView, i);
-    return;
-  }
-  const {position: {x, y, z}, size, colour: {r, g, b, a}} = hexPoint3d;
-  setHexPoint3dFlat(dataView, i, x, y, z, size, r, g, b, a);
-}
-
-export function setHexPoints3d(dataView, hexPoints3d) {
-  for (let i = 0; i < hexPoints3d.length; ++i) {
-    setHexPoint3d(dataView, i, hexPoints3d[i]);
-  }
-}
 
 export class HexContext3d {
   constructor({canvas, pixelSize=1, antialias=true, zMin=1, zMax=1000000, zDiv=800}) {
@@ -170,19 +143,37 @@ class HexLines3d  {
     this.dataView = new DataView(this.buffer);
   }
 
-  addPoint(hexPoint3d) {
+  addPointFlat(x, y, z, size, r, g, b, a) {
     this.dirty = true;
     this.ensureCapacity(this.length + 1);
-    setHexPoint3d(this.dataView, this.length, hexPoint3d);
+    this.dataView.setFloat32(this.length * kBytesPerHexPoint3d + 0, x, kLittleEndian);
+    this.dataView.setFloat32(this.length * kBytesPerHexPoint3d + 4, y, kLittleEndian);
+    this.dataView.setFloat32(this.length * kBytesPerHexPoint3d + 8, z, kLittleEndian);
+    this.dataView.setFloat32(this.length * kBytesPerHexPoint3d + 12, size, kLittleEndian);
+    this.dataView.setUint32(this.length * kBytesPerHexPoint3d + 16, rgbaToUint32(r, g, b, a), kLittleEndian);
     ++this.length;
   }
 
-  addPoints(hexPoints3d) {
+  addNull() {
     this.dirty = true;
+    this.ensureCapacity(this.length + 1);
+    this.dataView.setFloat32(this.length * kBytesPerHexPoint3d + 12, 0, kLittleEndian);
+    ++this.length;
+  }
+
+  addPoint(hexPoint3d) {
+    if (hexPoint3d === null) {
+      this.addNull();
+      return;
+    }
+    const {position: {x, y, z}, size, colour: {r, g, b, a}} = hexPoint3d;
+    this.addPointFlat(x, y, z, size, r, g, b, a);
+  }
+
+  addPoints(hexPoints3d) {
     this.ensureCapacity(this.length + hexPoints3d.length);
     for (const hexPoint3d of hexPoints3d) {
-      setHexPoint3d(this.dataView, this.length, hexPoint3d);
-      ++this.length;
+      this.addPoint(hexPoint3d);
     }
   }
 
