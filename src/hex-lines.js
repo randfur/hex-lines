12d84545e3f050f29469956d61528b552467b70d
Alan Cutter <randfur@gmail.com>
2023-11-19 23:12:06 +1100

    tan

--- src/hex-lines.js
+++ src/hex-lines.js
@@ -10,7 +10,7 @@ export class HexLinesContext {
       padding: 0;
       margin: 0;
       overflow: hidden;
-      touch-action: manipulation;
+      touch-action: none;
     `;
     const width = window.innerWidth;
     const height = window.innerHeight;
